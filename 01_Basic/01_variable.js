30cf39d9fadcd3b73318ba0c92170b61063b42c9
vijay2004k <kumbharvijay004@gmail.com>
2025-04-15 15:23:54 +0000

    variable

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,13 @@
+const  accountId=144453;
+let accountEmail="abc@gmail.com";
+var accountPassword="12345";
+accountCity="Jaipur";
+
+accountEmail="vijay@123";
+accountPassword="14586";
+accountCity="pune";
+
+// accountId=2; //Not Allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
